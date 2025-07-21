868b3a39010258d102c9cf9154c5784f5678f3ae
Bekzhan Kaspakov <bekzhan.kaspakov@nu.edu.kz>
2025-07-21 15:14:22 +0500

    trigger workflow

--- next/src/widgets/workouts-list/ui/workouts-list.tsx
+++ next/src/widgets/workouts-list/ui/workouts-list.tsx
@@ -9,14 +9,24 @@ import Image from "next/image";
 import { WorkoutsListProps } from "../lib/types";
 import { EditSetsDrawer } from "@/widgets/edit-sets";
 import { useState } from "react";
-import { useDeleteWorkout, useWorkouts, WORKOUT_QUERY_KEYS, WorkoutRecord } from "@/shared/api/workouts";
+import {
+  useDeleteWorkout,
+  useWorkouts,
+  WORKOUT_QUERY_KEYS,
+  WorkoutRecord,
+} from "@/shared/api/workouts";
 import { Spinner } from "@/shared/ui-kit/spinner";
-import { SwipeableList, SwipeableListItem, SwipeAction, TrailingActions } from 'react-swipeable-list'
+import {
+  SwipeableList,
+  SwipeableListItem,
+  SwipeAction,
+  TrailingActions,
+} from "react-swipeable-list";
 import { queryClient } from "@/shared/api/query-client";
 import { format } from "date-fns";
 
 export const WorkoutsList = ({ selectedDate }: WorkoutsListProps) => {
-  const deleteWorkout = useDeleteWorkout()
+  const deleteWorkout = useDeleteWorkout();
 
   const { data: workouts, isLoading } = useWorkouts(selectedDate);
 
@@ -37,9 +47,9 @@ export const WorkoutsList = ({ selectedDate }: WorkoutsListProps) => {
             `${selectedDate.getUTCMonth()} ${selectedDate.getUTCFullYear()}`,
           ),
         });
-      }
-    })
-  }
+      },
+    });
+  };
 
   const trailingActions = (workoutId: string) => (
     <TrailingActions>
@@ -62,9 +72,7 @@ export const WorkoutsList = ({ selectedDate }: WorkoutsListProps) => {
     <section className="flex flex-col gap-y-1 flex-1">
       <SwipeableList>
         {workouts?.map((x) => (
-          <SwipeableListItem key={x.id}
-            trailingActions={trailingActions(x.id)}
-          >
+          <SwipeableListItem key={x.id} trailingActions={trailingActions(x.id)}>
             <Accordion type="single" collapsible className="px-6 w-full">
               <AccordionItem value="item-1">
                 <AccordionTrigger className="py-3 items-center">
@@ -80,31 +88,32 @@ export const WorkoutsList = ({ selectedDate }: WorkoutsListProps) => {
                 </AccordionTrigger>
                 <AccordionContent>
                   <ol className="flex flex-col gap-y-1">
-                    {x.sets.length > 0 ? x.sets.map(([weight, reps], index) => (
+                    {x.sets.length > 0 ? (
+                      x.sets.map(([weight, reps], index) => (
+                        <li
+                          key={`${x.id}-${index}`}
+                          className="flex gap-x-2 items-center"
+                          onClick={() => setSelectedWorkout(x)}
+                        >
+                          <p className="px-4">{index + 1}</p>
+                          <Button variant="secondary" className="flex-1">
+                            {weight}
+                          </Button>
+                          <Button variant="secondary" className="flex-1">
+                            {reps}
+                          </Button>
+                        </li>
+                      ))
+                    ) : (
                       <li
-                        key={`${x.id}-${index}`}
                         className="flex gap-x-2 items-center"
                         onClick={() => setSelectedWorkout(x)}
                       >
-                        {" "}
-                        <p className="px-4">{index + 1}</p>
-                        <Button variant="secondary" className="flex-1">
-                          {weight}
-                        </Button>
-                        <Button variant="secondary" className="flex-1">
-                          {reps}
-                        </Button>
+                        <p className="px-4">+</p>
+                        <Button variant="secondary" className="flex-1"></Button>
+                        <Button variant="secondary" className="flex-1"></Button>
                       </li>
-                    )) : (<li
-                      className="flex gap-x-2 items-center"
-                      onClick={() => setSelectedWorkout(x)}
-                    >
-                      <p className="px-4">+</p>
-                      <Button variant="secondary" className="flex-1">
-                      </Button>
-                      <Button variant="secondary" className="flex-1">
-                      </Button>
-                    </li>)}
+                    )}
                   </ol>
                 </AccordionContent>
               </AccordionItem>
@@ -112,14 +121,12 @@ export const WorkoutsList = ({ selectedDate }: WorkoutsListProps) => {
           </SwipeableListItem>
         ))}
       </SwipeableList>
-      {
-        selectedWorkout && (
-          <EditSetsDrawer
-            selectedWorkout={selectedWorkout}
-            setSelectedWorkout={setSelectedWorkout}
-          />
-        )
-      }
-    </section >
+      {selectedWorkout && (
+        <EditSetsDrawer
+          selectedWorkout={selectedWorkout}
+          setSelectedWorkout={setSelectedWorkout}
+        />
+      )}
+    </section>
   );
 };
